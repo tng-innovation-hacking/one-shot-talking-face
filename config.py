b228228ab2e920d184c3fa6388d4968d8056219a
camenduru <camenduru@users.noreply.huggingface.co>
2023-01-14 17:51:16 +0000

    one-shot-talking-face-20.04-t4

---
+++ config.py
@@ -0,0 +1,3 @@
+OPENFACE_POSE_EXTRACTOR_PATH = "/content/one-shot-talking-face/OpenFace/FeatureExtraction"
+GENERATOR_CKPT = "/content/one-shot-talking-face/checkpoints/generator.ckpt"
+AUDIO2POSE_CKPT = "/content/one-shot-talking-face/checkpoints/audio2pose.ckpt"
